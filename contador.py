8670428357633a78312b623d72cba6e4ec18cf92
Oscar Delcíd <odelcid-2017173@kinal.edu.gt>
2022-04-19 22:04:40 -0600

    Cambios finales al contador

--- contador.py
+++ contador.py
@@ -5,24 +5,28 @@ ventana = tkinter.Tk()
 ventana.geometry("450x300")
 fontStyle = tkFont.Font(size = 40)
 
+#Función sumar
 def suma():
     valor= int(contador["text"]) 
     contador["text"]  = (valor+ 1)
     valor = contador["text"]
     cambiar_color(valor)
 
+#Función restar
 def restar():
     valor= int(contador["text"])
     contador["text"] = f"{valor-1}"
     valor = contador["text"]
     cambiar_color(valor)
-    
+
+#Función Reinciar    
 def reiniciar():
     valor= int(contador["text"])
     contador["text"] = (valor*0)
     valor = contador["text"]
     cambiar_color(valor)
 
+#Función Cambiar Color
 def cambiar_color(valor):
     if int(valor) < 0:
         contador.config(fg= "red")
@@ -32,16 +36,20 @@ def cambiar_color(valor):
         contador.config(fg= "black")
 
 
+#botones
 
 contador = tkinter.Label(ventana, text = "0", font=fontStyle )
 contador.place(x=210, y=100)
 
+#Boton Sumar
 btn_1 = tkinter.Button(ventana, text = "Sumar", width=20, height=-4,  command=suma)
 btn_1.place(x=40, y=200)
 
+#Boton Reiniciar
 btn_2 = tkinter.Button(ventana, text = "Reiniciar", width=20, height=-4, command=reiniciar)
 btn_2.place(x=150, y=200)
 
+#Boton Restar
 btn_3 = tkinter.Button(ventana, text = "Restar", width=20, height=-4, command=restar)
 btn_3.place(x=260, y=200)
 
